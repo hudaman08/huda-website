2d0ae17ed0aeca07cce881f53d31882be5236fd7
shawn huda <shawn.huda@gmail.com>
2017-11-18 00:07:58 -0500

    next & prev buttons

--- js/photos.js
+++ js/photos.js
@@ -2,6 +2,29 @@ $(document).ready(function() {
 
   var currentPhoto = "1";
 
+  $("div.prev-photo").mouseenter(function(){
+    $(this).addClass("navigation");
+    $("#prev").show();
+  });
+
+  $("div.prev-photo").mouseout(function(){
+    $(this).removeClass("navigation");
+    $("#prev").hide();
+  });
+
+  $("div.next-photo").mouseenter(function(){
+    $(this).addClass("navigation");
+    $("#next").show();
+    // alert("success");
+  });
+
+  $("div.next-photo").mouseout(function(){
+    $(this).removeClass("navigation");
+    $("#next").hide();
+    // alert("success");
+  });
+
+
   $("div.prev-photo").click(function() {
 
     if (parseInt(currentPhoto) === 1) {
