44c660041fb0b8f20701dc127f3969383854cdf5
shawn huda <shawn.huda@gmail.com>
2017-11-22 22:04:22 -0500

    beg of variable logic

--- js/xmas.js
+++ js/xmas.js
@@ -1,12 +1,68 @@
 $(document).ready(function() {
 
-  var hint = 0;
+  var hint = 1;
   var level = 1;
+  var givenAnswer;
+  var correctAnswer = $("#A"+level).html();
 
-  $("div.play-game").click(function() {
+
+  function checkAnswer() {
+    var givenAnswer = $("#answer").val();
+    if (givenAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+      rightAnswer();
+    } else {
+      alert(level);
+    }
+    console.log(correctAnswer);
+
+  }
+
+  function rightAnswer() {
+    alert("success");
+    level = parseInt(level) + 1;
+    hint = 0;
+    $("div.question").hide(function() {
+      $("div.hint").hide(function() {
+        $("div#Q"+level).show();
+      });
+    });
+  }
+
+  function wrongAnswer() {
+    alert("failure");
+    hint = parseInt(hint) + 1;
+
+  }
+
+  function showQuestion() {
+    $("div.question").hide(function() {
+      $("div.hint").hide(function() {
+        $("div#Q"+level).show();
+
+    });
+  });
+  }
+
+  function showHint() {
+    if (hint !== 0) {
+      $("div.hint").hide(function() {
+        $("div.hint.Q"+level+".H"+hint).delay().show();
+        showQuestion();
+      });
+    }
+  }
+
+  $("#submit-button").click(checkAnswer);
+
+  showHint();
+
+  $("#play-game").click(function() {
     $("div.video-wrapper").fadeOut(3000);
-    $("div#Q1").show();
+    showQuestion();
+    // $("div#Q" + level).show();
+
     $("div.game-wrapper").delay(3000).slideDown(3000);
+    level = 1;
 
   });
 
